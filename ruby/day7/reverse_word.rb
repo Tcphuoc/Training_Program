60bc385fb4af133caa551f90d0f66171767611bd
Tcphuoc <tcphuoc0511@gmail.com>
2023-08-25 11:37:46 +0700

    Ruby - CTDL&GT - Update reverse word

--- ruby/day7/reverse_word.rb
+++ ruby/day7/reverse_word.rb
@@ -6,9 +6,20 @@ def reverse_string(string)
   result
 end
 
-print 'Nhập chuỗi muốn đảo ngược: '
-string = gets.chomp
-puts "Sau khi đảo ngược: #{reverse_string(string)}"
+def reverse_array(array)
+  stack = []
+  array.each { |e| stack.push(e) }
+  result = []
+  stack.length.times {result << stack.pop}
+  result
+end
+
+# print 'Nhập chuỗi muốn đảo ngược: '
+# string = gets.chomp
+# puts "Sau khi đảo ngược: #{reverse_string(string)}"
+
+array = [1, 2, 3, 4, 5]
+puts "Sau khi đảo ngược: #{reverse_array(array)}"
 
 # Phép toán cơ bản: thêm và bớt phần tử vào stack
 # Số lượng: 2n vì cần n lần để thêm và n lần để bớt phần tử khỏi stack
