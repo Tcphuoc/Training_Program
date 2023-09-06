e24f6cf0a913470a59f38be842563c45f0cc79bc
Tcphuoc <tcphuoc0511@gmail.com>
2023-09-06 16:58:08 +0700

    Ruby - Principles - Update code

--- ruby/day10/principles/database.rb
+++ ruby/day10/principles/database.rb
@@ -7,34 +7,7 @@ class Database
     @database_name = database_name
   end
 
-  def insert_account(user_params)
-    return puts 'Email already exists, please choose another!' if account_existed?(user_params[:email])
-
-    # Lưu user account vào database
-    find_database_instance.execute('INSERT INTO users (email, password, name) VALUES (:email, :password, :name)',user_params)
-    find_database_instance.close
-  end
-
-  def update_account(user_params)
-    return puts 'Email does not exists!' unless account_existed?(user_params[:email])
-
-    # Update user account trong database
-    find_database_instance.execute('UPDATE users SET password = :password, name = :name WHERE email = :email',user_params)
-    find_database_instance.close
-  end
-
-  def find_account(email)
-    users = find_database_instance.execute('SELECT * FROM users WHERE email = :email', email: email)
-    users.first
-  end
-
-  private
-
   def find_database_instance
     SQLite3::Database.new(database_name)
-  end
-
-  def account_existed?(email)
-    return true if find_account(email)
   end
 end
--- ruby/day10/principles/exercise.rb
+++ ruby/day10/principles/exercise.rb
@@ -1,9 +1,9 @@
 require './user'
 require './address'
 
-User.new('john_henry@gmail.com', 'password', 'John', 'Harley')
+user = User.new('john_henry2@gmail.com', 'password', 'Mark', 'Henry')
 # user.create_user
 # user.update_user
 
-Address.new('TP.HCM', 'Q12, CVPM Quang Trung')
+address = Address.new('TP.HCM', 'Q12, CVPM Quang Trung')
 # address.print_user_info_and_address(user)
